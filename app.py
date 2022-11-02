0955c8b60de1506fc3d27365209f80ae6f5dad55
alicenkbaytop <baytop.alicenk@gmail.com>
2022-11-02 18:07:27 +0300

    selectbox title has changed

--- app.py
+++ app.py
@@ -2,18 +2,7 @@ import streamlit as st
 from predict_page import show_predict_page
 from details_page import show_details_page
 
-st.markdown(
-    """
-    <style>
-    body {
-        background: #edf2f7;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-page = st.sidebar.selectbox("Details Or Predict", ("Predict", "Details"))
+page = st.sidebar.selectbox("Predict Or Details", ("Predict", "Details"))
 
 if page == "Predict":
     show_predict_page()
