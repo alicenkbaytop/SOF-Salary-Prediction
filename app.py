3163ba0f5aeb0f8e46ec5f7959b4ded9f5116a38
alicenkbaytop <baytop.alicenk@gmail.com>
2022-11-02 17:48:37 +0300

    updated background color

--- app.py
+++ app.py
@@ -2,9 +2,23 @@ import streamlit as st
 from predict_page import show_predict_page
 from details_page import show_details_page
 
-with open('style.css') as f:
+"""with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-    
+"""
+st.markdown(
+    """
+    <style>
+    * {
+        background-color: #8d4141;
+        border: 2px solid #CCCCCC;
+        padding: 5% 5% 5% 10%;
+        border-radius: 5px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 page = st.sidebar.selectbox("Details Or Predict", ("Predict", "Details"))
 
 if page == "Predict":
